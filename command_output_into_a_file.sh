4af5b9651bb237ef5b647d9f14f3ed33c084e73c
Indika Bandara <40309775+iwbandara@users.noreply.github.com>
2022-10-25 07:20:45 +0530

    Add files via upload

---
+++ command_output_into_a_file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Author: Indika W Bandara
+#Date: 25th of October 2022
+#Version: 1.0
+
+# Script to store command output into a file
+# This will create a file if not exists, otherwise overwrite the content
+
+ls > ls_output.txt
