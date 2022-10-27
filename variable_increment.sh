dee4b5873802ee78583ead1eac04057d6c5fbe85
Indika Bandara <40309775+iwbandara@users.noreply.github.com>
2022-10-27 09:51:50 +0530

    Add files via upload

---
+++ variable_increment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Increment of variable values
+
+i=1
+
+# Method : 1
+((i=i+1))
+
+# Method : 2
+i=$((i+1))
+
+# Method : 3
+let "i=i+1"
+
+# Use of while loop to display numbers from 1 to 5
+a=1
+
+while [ $a -le 5 ]
+do
+	echo "i value is : $a"
+	((a=a+1))
+done
