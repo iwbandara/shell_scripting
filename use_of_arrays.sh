a0760a03582265872d6b11a8b488f450ea1cbac5
Indika W Bandara <40309775+iwbandara@users.noreply.github.com>
2022-11-13 07:48:04 +0530

    Update use_of_arrays.sh

--- use_of_arrays.sh
+++ use_of_arrays.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #Author: Indika W Bandara
-#Date: 2022-11-01
-#Version: 1.0
+#DateCreated: 2022-11-01
+#DatedLastEdited: 2022-11-13
+#Version: 1.1
 
 # Use of arrays in Shell Scripts
 
@@ -14,6 +15,15 @@ echo ${str_array[@]}
 
 # Display the count of the array elements
 echo ${#str_array[@]}
+
+# Adding a new element to the end of the array from user input
+read -p "Enter the next element value : " element_value
+count=${#str_array[@]}
+str_array[$count]=$element_value
+
+# How to remove an element form the array
+# Use of "unset" keyword
+unset str_array[1] # removes the second element of the array
 
 # Creating a new array form a command output
 ls_array=($(ls)) #creates an array from the output of 'ls' command
